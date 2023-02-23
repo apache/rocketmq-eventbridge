2472761e19bbfc595bd18b4fa77c919f3771be15
Artisan <AmbooChou@gmail.com>
2023-02-23 21:30:07 +0800

    updateModuleDivide

--- adapter/runtimer/src/main/java/org/apache/rocketmq/eventbridge/adapter/runtimer/boot/EventBusListener.java
+++ adapter/runtimer/src/main/java/org/apache/rocketmq/eventbridge/adapter/runtimer/boot/EventBusListener.java
@@ -3,7 +3,7 @@ package org.apache.rocketmq.eventbridge.adapter.runtimer.boot;
 /**
  * 运行器通道
  */
-public class RuntimerChannel implements Runnable{
+public class EventBusListener implements Runnable{
 
     @Override
     public void run() {
--- adapter/runtimer/src/main/java/org/apache/rocketmq/eventbridge/adapter/runtimer/boot/EventRuleTransfer.java
+++ adapter/runtimer/src/main/java/org/apache/rocketmq/eventbridge/adapter/runtimer/boot/EventRuleTransfer.java
@@ -1,6 +1,6 @@
 package org.apache.rocketmq.eventbridge.adapter.runtimer.boot;
 
-public class Transformer {
+public class EventRuleTransfer {
 
     public void init(){
 
