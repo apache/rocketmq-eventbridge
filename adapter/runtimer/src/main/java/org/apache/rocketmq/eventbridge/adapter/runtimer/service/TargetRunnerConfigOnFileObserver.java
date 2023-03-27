701cdac9033d2e0bf157b98c406136e49913c198
shenlin <2011shenlin@gmail.com>
2023-03-27 16:04:30 +0800

    feat:add watch runner config. (#63)
    feat:add watch runner config.

--- adapter/runtimer/src/main/java/org/apache/rocketmq/eventbridge/adapter/runtimer/service/TargetRunnerConfigOnFileObserver.java
+++ adapter/runtimer/src/main/java/org/apache/rocketmq/eventbridge/adapter/runtimer/service/TargetRunnerConfigOnFileObserver.java
@@ -17,51 +17,78 @@
 
 package org.apache.rocketmq.eventbridge.adapter.runtimer.service;
 
+import com.google.common.collect.Maps;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.apache.rocketmq.common.utils.ThreadUtils;
 import org.apache.rocketmq.eventbridge.adapter.runtimer.common.entity.TargetRunnerConfig;
-import org.apache.rocketmq.eventbridge.adapter.runtimer.common.plugin.Plugin;
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.rocketmq.eventbridge.exception.EventBridgeException;
 
 @Slf4j
 public class TargetRunnerConfigOnFileObserver extends AbstractTargetRunnerConfigObserver {
 
-    @Value("${runtimer.storePathRootDir:}")
-    private String storeRootPath;
-
-    @Value("${runtimer.store.targetRunner.config:targetRunner-config}")
-    private String fileName;
+    private String pathName;
 
     private static ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor(
         ThreadUtils.newThreadFactory("TargetRunnerConfigOnFileObserver", false));
 
-    public TargetRunnerConfigOnFileObserver(Plugin plugin) {
-        this.addListen(storeRootPath, fileName, this);
+    public TargetRunnerConfigOnFileObserver(String pathName) {
+        this.pathName = pathName;
+        super.getTargetRunnerConfig().addAll(getLatestTargetRunnerConfig());
+        this.addListen(pathName, this);
+    }
+
+    public TargetRunnerConfigOnFileObserver() {
+        super.getTargetRunnerConfig().addAll(getLatestTargetRunnerConfig());
+        this.addListen(pathName, this);
     }
 
     @Override
     public Set<TargetRunnerConfig> getLatestTargetRunnerConfig() {
-        return null;
+        String config = null;
+        try {
+            File file = new File(pathName);
+            config = FileUtils.readFileToString(file, "UTF-8");
+            Type workerConfigType = new TypeToken<HashSet<TargetRunnerConfig>>() {
+            }.getType();
+            Set<TargetRunnerConfig> taskConfigList = new Gson().fromJson(config, workerConfigType);
+            return taskConfigList;
+        } catch (IOException e) {
+            throw new EventBridgeException("Load component properties failed.", e);
+        } catch (Throwable e) {
+            log.error("fail to parse config={} from file={}", config, pathName);
+            throw e;
+        }
     }
 
-    public void addListen(String pathStr, String fileName,
+    public void addListen(String pathName,
         TargetRunnerConfigOnFileObserver pusherConfigOnFileService) {
-        log.info("Watching task file changing:{}", pathStr + fileName);
+        log.info("Watching task file changing:{}", pathName);
+        int index = pathName.lastIndexOf("/");
+        String filePath = pathName.substring(0, index);
+        String fileName = pathName.substring(index + 1);
         service.scheduleAtFixedRate(() -> {
             try {
                 WatchService watchService = FileSystems.getDefault()
                     .newWatchService();
-                Path path = Paths.get(pathStr);
+                Path path = Paths.get(filePath);
                 path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE,
                     StandardWatchEventKinds.ENTRY_MODIFY);
                 WatchKey watchKey;
@@ -81,12 +108,32 @@ public class TargetRunnerConfigOnFileObserver extends AbstractTargetRunnerConfig
     }
 
     public void diff() {
-        Set<TargetRunnerConfig> latest = this.getLatestTargetRunnerConfig();
-        Set<TargetRunnerConfig> last = super.getTargetRunnerConfig();
-        TargetRunnerConfig changed = null;
-        super.onAddTargetRunner(changed);
-        super.onUpdateTargetRunner(changed);
-        super.onDeleteTargetRunner(changed);
+        Map<String, TargetRunnerConfig> lastMap = toMap(super.getTargetRunnerConfig());
+        Map<String, TargetRunnerConfig> latestMap = toMap(this.getLatestTargetRunnerConfig());
+        lastMap.entrySet().forEach(entry -> {
+            TargetRunnerConfig latest = latestMap.get(entry.getKey());
+            if (latest == null) {
+                super.onDeleteTargetRunner(entry.getValue());
+            } else if (!latest.equals(entry.getValue())) {
+                super.onUpdateTargetRunner(entry.getValue());
+            }
+        });
+
+        latestMap.entrySet().forEach(entry -> {
+            TargetRunnerConfig latest = lastMap.get(entry.getKey());
+            if (latest == null) {
+                super.onAddTargetRunner(entry.getValue());
+            }
+        });
+    }
+
+    private Map<String, TargetRunnerConfig> toMap(Set<TargetRunnerConfig> targetRunnerConfigs) {
+        if (targetRunnerConfigs == null || targetRunnerConfigs.isEmpty()) {
+            return Maps.newHashMapWithExpectedSize(0);
+        }
+        Map<String, TargetRunnerConfig> map = Maps.newHashMap();
+        targetRunnerConfigs.forEach(entry -> map.put(entry.getName(), entry));
+        return map;
     }
 
 }
