da888546381fa2d2a7860283dbb887387a6ca083
zhaohai <33314633+zhaohai666@users.noreply.github.com>
2023-03-29 11:10:07 +0800

    Update api list connection name (#68)
    Update api list connection name

--- adapter/api/src/main/java/org/apache/rocketmq/eventbridge/adapter/api/dto/apidestination/ListApiDestinationsRequest.java
+++ adapter/api/src/main/java/org/apache/rocketmq/eventbridge/adapter/api/dto/apidestination/ListApiDestinationsRequest.java
@@ -33,8 +33,8 @@ public class ListApiDestinationsRequest extends BaseResponse {
     @SerializedName("ApiDestinationNamePrefix")
     private String apiDestinationNamePrefix;
 
-    @SerializedName("ConnectionNamePrefix")
-    private String connectionNamePrefix;
+    @SerializedName("ConnectionName")
+    private String connectionName;
 
 
     @Min(value = 0, message = "The limit size of page is invalid, which must greater than 0 and less than [{0}].")
