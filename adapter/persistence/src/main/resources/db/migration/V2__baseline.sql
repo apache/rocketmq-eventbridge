6afebb0a8ca5324387c5c3c614d62ca2a72955fc
wangkai <wangkai@zhongan.com>
2023-06-05 20:03:15 +0800

    1.merge h2-migration and migration

--- adapter/persistence/src/main/resources/db/migration/V2__baseline.sql
+++ adapter/persistence/src/main/resources/db/migration/V2__baseline.sql
@@ -1,32 +1,34 @@
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 CREATE TABLE IF NOT EXISTS `event_connection` (
     `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
     `account_id` varchar(255) NOT NULL COMMENT 'event_connection account id',
     `name` varchar(128) NOT NULL DEFAULT '' COMMENT '名称',
-    `authorization_type` varchar(128) DEFAULT NULL COMMENT '授权类型',
-    `auth_parameters` text DEFAULT NULL COMMENT '',
+    `authorization_type` varchar(128) NULL DEFAULT '' COMMENT '授权类型',
+    `auth_parameters` text NULL DEFAULT NULL COMMENT '',
     `network_type` varchar(128) NOT NULL DEFAULT '' COMMENT '网络类型',
     `network_parameters` text DEFAULT NULL COMMENT '网络配置',
     `description` varchar(255) DEFAULT NULL COMMENT 'a description about the event_connection',
     `gmt_create` datetime DEFAULT NULL COMMENT 'create time',
     `gmt_modify` datetime DEFAULT NULL COMMENT 'modify time',
     PRIMARY KEY (`id`),
-    UNIQUE KEY `name_uniq_key` (`name`)
-    ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='event connection meta'
+    UNIQUE KEY `name_uniq_key_event_connection` (`name`)
+    ) ENGINE=InnoDB  DEFAULT CHARSET=utf8
 ;
 
 
@@ -42,6 +44,6 @@ CREATE TABLE IF NOT EXISTS `event_api_destination` (
     `gmt_create` datetime DEFAULT NULL COMMENT 'create time',
     `gmt_modify` datetime DEFAULT NULL COMMENT 'modify time',
     PRIMARY KEY (`id`),
-    UNIQUE KEY `name_uniq_key` (`name`)
-    ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='event api destination meta'
+    UNIQUE KEY `name_uniq_key_event_api_destination` (`name`)
+    ) ENGINE=InnoDB  DEFAULT CHARSET=utf8
 ;
--- docs/cn/RocketMQEventBridgeDatabaseUsage.md
+++ docs/cn/RocketMQEventBridgeDatabaseUsage.md
@@ -7,15 +7,17 @@ RocketMQ EventBridge 使用数据库存储运行过程中定义的规则
 * 内置数据库h2，主要方便进行本地测试和快速开始
 * mysql数据库，实际生产部署时可以使用
 
-### 内置数据库
+### 内置数据库H2
+#### H2介绍
+H2是一个使用Java实现的内存数据库，支持标准的SQL语法，支持大部分的MySQL语法和函数
+
 ```properties
 # 是否开启本地页面访问
 spring.profiles.active=local
-# h2 sql migration所在目录
-spring.flyway.locations=classpath:db/h2-migration
 ```
-本地运行后，访问h2数据库。访问url: localhost:8083：
+本地运行后，访问h2数据库。访问url: localhost:8083，用户名/密码: sa/sa：
 ![img.png](images/h2.png)
+
 
 ### mysql数据库
 ```properties
@@ -24,6 +26,4 @@ spring.datasource.hikari.jdbc-url=jdbc:mysql://localhost:3306/eventBridge?useUni
 spring.datasource.hikari.driver-class-name=com.mysql.jdbc.Driver
 spring.datasource.hikari.username=xxxxx
 spring.datasource.hikari.password=xxxxx
-# 注释h2使用的sql migration
-#spring.flyway.locations=classpath:db/h2-migration
 ```
