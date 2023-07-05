a6fb646973a188bf1e93df24ee69504ad121e5f2
chengxy <chengxy@qk.com>
2023-07-05 11:52:20 +0800

    add license && remove useless code

--- metrics/src/main/java/org/apache/rocketmq/eventbridge/BridgeMetricsConstant.java
+++ metrics/src/main/java/org/apache/rocketmq/eventbridge/BridgeMetricsConstant.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.apache.rocketmq.eventbridge;
 
 public class BridgeMetricsConstant {
@@ -15,8 +32,4 @@ public class BridgeMetricsConstant {
 
     /** eventbridge process message latency**/
     public static final String HISTOGRAM_RPC_LATENCY = "process_latency";
-
-    public static final String LABEL_AGGREGATION = "aggregation";
-    public static final String AGGREGATION_DELTA = "delta";
-    public static final String LABEL_PROCESSOR = "processor";
 }
