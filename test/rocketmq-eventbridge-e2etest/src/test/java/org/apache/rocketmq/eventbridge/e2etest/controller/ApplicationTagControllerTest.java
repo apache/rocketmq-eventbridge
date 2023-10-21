5426245f2652e7c4ea2c274bea5b4f92eccb783e
wangkai <wangkai@zhongan.com>
2023-10-21 11:21:33 +0800

    [e2eTest] add e2eTest model

--- test/rocketmq-eventbridge-e2etest/src/test/java/org/apache/rocketmq/eventbridge/e2etest/controller/ApplicationTagControllerTest.java
+++ test/rocketmq-eventbridge-e2etest/src/test/java/org/apache/rocketmq/eventbridge/e2etest/controller/ApplicationTagControllerTest.java
@@ -1,5 +1,7 @@
 package org.apache.rocketmq.eventbridge.e2etest.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.apache.rocketmq.eventbridge.Main;
 import org.apache.rocketmq.eventbridge.e2etest.BaseTest;
 import org.apache.rocketmq.eventbridge.e2etest.util.Utils;
@@ -11,21 +13,45 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Main.class)
 public class ApplicationTagControllerTest extends BaseTest {
 
     @Test
-    public void testGetAllApplicationTags() {
-        String url = String.format("%s/bus/createEventBus", baseUrl);
-        Map<String, String> maps = new HashMap<>();
-        maps.put("eventBusName", "demo-bus");
-        maps.put("description", "a demo bus.");
-        ResponseEntity<Void> response = Utils.request(template, url, HttpMethod.POST, maps, Void.class);
+    public void testGetAllApplicationTags() throws Exception {
+
+        String url = String.format("%s/putEvents", baseUrl);
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json; charset=UTF-8");
+        headers.put("ce-specversion","1.0");
+        headers.put("ce-type", "com.github.pull_request.opened");
+        headers.put("ce-source", "https://github.com/cloudevents/spec/pull");
+        headers.put("ce-subject", "demo");
+        headers.put("ce-id", "1234-1234-1234");
+        headers.put("ce-datacontenttype", "application/json");
+        headers.put("ce-time","2018-04-05T17:31:00Z");
+        headers.put("ce-eventbusname", "demo-bus");
+
+        ResponseEntity<Void> response = Utils.request(template, url, HttpMethod.POST, "A test recrod.", Void.class, headers);
         Assert.assertTrue(response.getStatusCode().is2xxSuccessful());
+
+        File file = new File(System.getProperty("user.home") + "/demo");
+        String data = null;
+        long current = System.currentTimeMillis();
+        while((System.currentTimeMillis() - current)/1000 <300 ) {
+            data = FileUtils.readFileToString(file, "UTF-8");
+            if (data != null) {
+                break;
+            }
+            Thread.sleep(1000);
+        }
+
+        Assert.assertEquals("A test recrod.\n", data);
     }
 
 
